5465db3b979a6d32e54556b134fb871a1ff51643
hs0907j <37132917+hs0907j@users.noreply.github.com>
2018-03-08 22:17:21 +0900

    Add files via upload

---
+++ Blink.cpp
@@ -0,0 +1,16 @@
+#include "Blink.h"
+
+Blink::Blink(int input) {
+	pinNumber = input;
+	pinMode(pinNumber, OUTPUT);
+}
+
+void Blink::on(int onTime) {
+	digitalWrite(pinNumber, LOW); 
+	delay(onTime);
+}
+
+void Blink::off(int offTime) {
+	digitalWrite(pinNumber, HIGH);
+	delay(offTime);
+}
---
+++ Blink.h
@@ -0,0 +1,15 @@
+#ifndef Blink_H
+#define Blink_H
+
+class Blink {
+private: 
+	int pinNumber;
+
+public:
+	Blink(int input);
+
+	void on(int onTime);
+	void off(int outTime);
+};
+
+#endif
